a72d37c34945b1ee96846ef2377172dfe0922345
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-08-13 19:25:53 +0600

    Everything is working as expected. Code is messy and design is somewhat crappy, but it's completely functional now.

--- js/script.js
+++ js/script.js
@@ -14,11 +14,16 @@ $(document).ready(function() {
 				$(".insert-quote").html(quotes.quote);
 				$(".insert-author").html("- " + quotes.author);
 				$("#tweet").attr("href", "https://twitter.com/intent/tweet?text="+quotes.quote+" -"+quotes.author);
+				$("#quote-box").attr("style", "background-color: #8F"+(Math.floor(Math.random() * (99 - 10 + 1) + 10))+(Math.floor(Math.random() * (99 - 10 + 1) + 10))+";");
 			},
 			beforeSend: function(xhr) {
 		   xhr.setRequestHeader("X-Mashape-Authorization", "tLoCsKtopemshNeZYqkzFIYU45dvp1WHQRrjsnmBOsG4EMUvoE");
 		  }
 		});
+	});
+
+	$("#btn").on("click", function(){
+		$(".refresh").toggleClass("rotate");
 	});
 
 });
