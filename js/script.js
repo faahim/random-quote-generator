540380dba112e5e7a876143e26dc71813793d072
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-08-13 13:44:49 +0600

    The skeleton now loads random quotes. Still undesigned.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+	$("#btn").on("click", function(){
+		
+		$.ajax({
+			url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+			type: "GET",
+			data: {
+				cat: "famous",
+				count: "1",
+			},
+			dataType: 'json',
+			success: function(quotes) {
+				$(".insert-quote").html(quotes.quote);
+				$(".insert-author").html("- " + quotes.author);
+			},
+			beforeSend: function(xhr) {
+		   xhr.setRequestHeader("X-Mashape-Authorization", "tLoCsKtopemshNeZYqkzFIYU45dvp1WHQRrjsnmBOsG4EMUvoE");
+		  }
+		});
+	});
+
+});
