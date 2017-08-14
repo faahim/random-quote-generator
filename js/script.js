f062ed55c93afa35e63e23063a2b478e6f5b9f02
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2017-08-14 13:23:01 +0600

    App is now responsive. Maybe now I can call this project finished now.

--- js/script.js
+++ js/script.js
@@ -15,6 +15,13 @@ $(document).ready(function() {
 				$(".insert-author").html("- " + quotes.author);
 				$("#tweet").attr("href", "https://twitter.com/intent/tweet?text="+quotes.quote+" -"+quotes.author);
 				$("#quote-box").attr("style", "background-color: #8F"+(Math.floor(Math.random() * (99 - 10 + 1) + 10))+(Math.floor(Math.random() * (99 - 10 + 1) + 10))+";");
+				if (quotes.quote.length > 100) {
+					$(".insert-quote").attr("style", "font-size: 2.5em");
+				} else if (quotes.quote.length > 160) {
+					$(".insert-quote").attr("style", "font-size: 2em");
+				} else if (quotes.quote.length < 100) {
+					$(".insert-quote").attr("style", "font-size: 3em");
+				}
 			},
 			beforeSend: function(xhr) {
 		   xhr.setRequestHeader("X-Mashape-Authorization", "tLoCsKtopemshNeZYqkzFIYU45dvp1WHQRrjsnmBOsG4EMUvoE");
